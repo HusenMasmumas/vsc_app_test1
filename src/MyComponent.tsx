3a7509bb5a46d96dde9c5df899cecb6715a1aefb
husen <husen@zicure.com>
2024-06-05 08:57:57 +0700

    test

---
+++ src/MyComponent.tsx
@@ -0,0 +1,11 @@
+// src/MyComponent.js
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const MyComponent = () => (
+  <View>
+    <Text>Hello from awesomeproject!</Text>
+  </View>
+);
+
+export default MyComponent;
